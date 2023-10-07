2ef3ed68943856c7a4981afefc9c320444cbbb72
Sunitha <136448809+SunithaBisinigi@users.noreply.github.com>
2023-10-07 15:41:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import CustomData from "./components/CustomData";
+import "./styles.css";
+import "devextreme/dist/css/dx.light.css";
+export default function App() {
+  return (
+    <div className="App">
+      <CustomData />
+    </div>
+  );
+}
